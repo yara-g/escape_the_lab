9ee354684ff992b4c1fd28f51ee2b5f7baf51d56
Yara Ghamlouch <ghamlouchyara68@gmail.com>
2025-03-03 10:45:00 -0500

    added room 1

---
+++ src/main/java/com/example/escape_the_lab/Room1.java
@@ -0,0 +1,12 @@
+package com.example.escape_the_lab;
+
+import javafx.scene.Scene;
+import javafx.scene.layout.FlowPane;
+
+public class Room1 {
+    public Scene makeScene() {
+        FlowPane pane = new FlowPane();
+        Scene scene = new Scene(pane);
+
+    }
+}
