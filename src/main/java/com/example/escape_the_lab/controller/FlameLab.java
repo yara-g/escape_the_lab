3164f6bb9d1a3d3916ff0dca36dae2d1f40f3a0d
ZovianSh <2389589@edu.vaniercollege.qc.ca>
2025-03-03 11:25:56 -0500

    lab rooms

--- src/main/java/com/example/escape_the_lab/controller/FlameLab.java
+++ src/main/java/com/example/escape_the_lab/controller/FlameLab.java
@@ -1,4 +1,27 @@
 package com.example.escape_the_lab.controller;
 
-public class FlameLab {
+public class FlameLab extends Lab{
+    // background
+    // image view for lab, drawer or other, door, 2 random useless things.
+    // lab tools, inside drawer, changes for things (swatch eyes in image, etc.)
+    // 1050/650
+    private String powder;
+
+    @Override
+    public void startLab() {
+
+    }
+
+    @Override
+    public boolean checkSolution() {
+        return false;
+    }
+
+    @Override
+    public void failLab() {
+
+    }
+    public void changeFlameColor() {
+
+    }
 }
