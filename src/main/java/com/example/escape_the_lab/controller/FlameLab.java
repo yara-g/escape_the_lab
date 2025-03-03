0fafebb65678c780c1437fd93f8228b3ceb24b42
JiangaiGao <6249771@edu.vaniercollege.qc.ca>
2025-03-03 12:36:47 -0500

    candle lab image view slots

--- src/main/java/com/example/escape_the_lab/controller/FlameLab.java
+++ src/main/java/com/example/escape_the_lab/controller/FlameLab.java
@@ -1,8 +1,55 @@
 package com.example.escape_the_lab.controller;
 
 import com.example.escape_the_lab.model.Lab;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 public class FlameLab extends Lab {
+    // Flame test
+    // Possible flames : 4 colors, right color is the wall paper.
+    private final ImageView flameColorCrimsonLab = new ImageView(new Image("file:")); //
+    private final ImageView flameColorGreenLab = new ImageView(new Image("file:")); //
+    private final ImageView flameColorLilacLab = new ImageView(new Image("file:")); //
+    private final ImageView flameColorYellowLab = new ImageView(new Image("file:")); //
+    // Chosen flame color.
+    // Collect the flame to put on a slot on door
+    // My hand! It burns! (wrong color)
+    // Thank God I don't have to play with fire anymore... (right color)
+    private ImageView flameColor;
+    private final ImageView flameColorCrimsonTool = new ImageView(new Image("file:"));
+    private final ImageView flameColorGreenTool = new ImageView(new Image("file:"));
+    private final ImageView flameColorLilacTool = new ImageView(new Image("file:"));
+    private final ImageView flameColorYellowTool = new ImageView(new Image("file:"));
+    // Powders to find from microscope (and they appear as the chemical formula)
+    // Are those...? (If inventory doesn't have powder yet)
+    private final ImageView licl = new ImageView(new Image("file:1.webp"));
+    private final ImageView bacl2 = new ImageView(new Image("file:"));
+    private final ImageView kcl = new ImageView(new Image("file:"));
+    private final ImageView nacl = new ImageView(new Image("file:"));
+    private final ImageView liclTool = new ImageView(new Image("file:1.webp"));
+    private final ImageView bacl2Tool = new ImageView(new Image("file:"));
+    private final ImageView kclTool = new ImageView(new Image("file:"));
+    private final ImageView naclTool = new ImageView(new Image("file:"));
+    // Bunsen burner blue flame. Found from
+    // Now let me cook.
+    private final ImageView bunsenBurner = new ImageView(new Image("file:"));
+    private final ImageView BunsenBurnerTool = new ImageView(new Image("file:"));
+    private final ImageView bunsenBurnerLab = new ImageView(new Image("file:"));
+    // Wire loop. Found from
+    private final ImageView wireLoop = new ImageView(new Image("file:"));
+    private final ImageView wireLoopTool = new ImageView(new Image("file:"));
+    private final ImageView wireLoopLab = new ImageView(new Image("file:"));
+    // Test tubes: 3, be on table.
+    // If try to put in used tube: It's already filled.
+    private final ImageView testTubesLab = new ImageView(new Image("file:"));
+    // Tiny paper hint on table says: "Look around you"
+    // Other images needed.
+    private final ImageView telescope = new ImageView(new Image("file:"));
+    // Close up of pics: "Big chemistry fan huh..."
+    private final ImageView wall = new ImageView(new Image("file:"));
+    private final ImageView bats = new ImageView(new Image("file:"));
+    private final ImageView batsFly = new ImageView(new Image("file:"));
+
     // background
     // image view for lab, drawer or other, door, 2 random useless things.
     // lab tools, inside drawer, changes for things (swatch eyes in image, etc.)
