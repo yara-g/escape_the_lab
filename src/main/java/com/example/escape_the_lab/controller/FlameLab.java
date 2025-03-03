0d103fc80f8ddab8f7d360e98726c202d8a9a094
JiangaiGao <6249771@edu.vaniercollege.qc.ca>
2025-03-03 12:10:47 -0500

    candle lab image view slots

--- src/main/java/com/example/escape_the_lab/controller/FlameLab.java
+++ src/main/java/com/example/escape_the_lab/controller/FlameLab.java
@@ -5,27 +5,50 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
 public class FlameLab extends Lab {
-    //Flame test
-    ImageView flameColorCrimson = new ImageView(new Image("file:")); //Li+
-    ImageView flameColorGreen = new ImageView(new Image("file:")); //Ba2+
-    ImageView flameColorLilac = new ImageView(new Image("file:")); //K+
-    ImageView flameColorYellow = new ImageView(new Image("file:")); //Na+
-
-    // Regulate a Bunsen burner <- find from room
-    // A clean wire loop <- find
-    // Test tubes: 3, be on table.
-    // Tiny paper hint on table says: "Look around you"
+    // Flame test
     // Possible flames : 4 colors, right color is the wall paper.
+    private final ImageView flameColorCrimsonLab = new ImageView(new Image("file:")); //LiCl
+    private final ImageView flameColorGreenLab = new ImageView(new Image("file:")); //BaCl2
+    private final ImageView flameColorLilacLab = new ImageView(new Image("file:")); //KCl
+    private final ImageView flameColorYellowLab = new ImageView(new Image("file:")); //NaCl
+    // Chosen flame color.
     // Collect the flame to put on a slot on door
-    // If same door opens if not burn your hand.
+    // My hand! It burns! (wrong color)
+    // Thank God I don't have to play with fire anymore... (right color)
+    private ImageView flameColor;
+    private final ImageView flameColorCrimsonTool = new ImageView(new Image("file:"));
+    private final ImageView flameColorGreenTool = new ImageView(new Image("file:"));
+    private final ImageView flameColorLilacTool = new ImageView(new Image("file:"));
+    private final ImageView flameColorYellowTool = new ImageView(new Image("file:"));
+    // Powders to find (just white)
+    // Are those...? (If inventory doesn't have powder yet
+    private final ImageView licl = new ImageView(new Image("file:1.webp"));
+    private final ImageView bacl2 = new ImageView(new Image("file:"));
+    private final ImageView kcl = new ImageView(new Image("file:"));
+    private final ImageView nacl = new ImageView(new Image("file:"));
+    private final ImageView liclTool = new ImageView(new Image("file:1.webp"));
+    private final ImageView bacl2Tool = new ImageView(new Image("file:"));
+    private final ImageView kclTool = new ImageView(new Image("file:"));
+    private final ImageView naclTool = new ImageView(new Image("file:"));
+    // Bunsen burner blue flame. Found from
+    // Now let me cook.
+    private final ImageView bunsenBurner = new ImageView(new Image("file:"));
+    private final ImageView BunsenBurnerTool = new ImageView(new Image("file:"));
+    private final ImageView bunsenBurnerLab = new ImageView(new Image("file:"));
+    // Wire loop. Found from
+    private final ImageView wireLoop = new ImageView(new Image("file:"));
+    private final ImageView wireLoopTool = new ImageView(new Image("file:"));
+    private final ImageView wireLoopLab = new ImageView(new Image("file:"));
+    // Test tubes: 3, be on table.
+    // If try to put in used tube: It's already filled.
+    private final ImageView testTubesLab = new ImageView(new Image("file:"));
+    // Tiny paper hint on table says: "Look around you"
 
     // background
     // image view for lab, drawer or other, door, 2 random useless things.
     // lab tools, inside drawer, changes for things (swatch eyes in image, etc.)
     // 1000/650
     // start/end
-
-    private String powder;
 
     @Override
     public void startLab() {
