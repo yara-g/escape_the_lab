2a55b09d0b573e7f4a2cc761265aba3b6b835269
Yara Ghamlouch <ghamlouchyara68@gmail.com>
2025-03-03 10:27:37 -0500

    test?

---
+++ src/main/java/com/example/escape_the_lab/CircuitLab.java
@@ -0,0 +1,21 @@
+package com.example.escape_the_lab;
+
+public class CircuitLab extends Lab{
+    private final String name = "Circuit Lab";
+
+
+    @Override
+    public void startLab() {
+
+    }
+
+    @Override
+    public boolean checkSolution() {
+        return false;
+    }
+
+    @Override
+    public void failLab() {
+
+    }
+}
