2ffbc7987164184cfad2daa45954f6318ac881a7
annesophiedesaulniers <2372588@edu.vaniercollege.qc.ca>
2025-03-03 11:04:54 -0500

    rearranging

--- src/main/java/com/example/escape_the_lab/controller/AcidNeutralizationLab.java
+++ src/main/java/com/example/escape_the_lab/controller/AcidNeutralizationLab.java
@@ -1,4 +1,25 @@
 package com.example.escape_the_lab.controller;
 
-public class AcidNeutralizationLab {
+import com.example.escape_the_lab.model.Lab;
+
+public class AcidNeutralizationLab extends Lab {
+    private String acid;
+    private String base;
+    @Override
+    public void startLab() {
+
+    }
+
+    @Override
+    public boolean checkSolution() {
+        return false;
+    }
+
+    @Override
+    public void failLab() {
+
+    }
+    public void mixSolutions() {
+
+    }
 }
--- src/main/java/com/example/escape_the_lab/controller/SpringLab.java
+++ src/main/java/com/example/escape_the_lab/controller/SpringLab.java
@@ -1,4 +1,24 @@
 package com.example.escape_the_lab.controller;
 
-public class SpringLab {
+import com.example.escape_the_lab.model.Lab;
+
+public class SpringLab extends Lab {
+    private double mass;
+    @Override
+    public void startLab() {
+
+    }
+
+    @Override
+    public boolean checkSolution() {
+        return false;
+    }
+
+    @Override
+    public void failLab() {
+
+    }
+    public void chooseMass() {
+
+    }
 }
