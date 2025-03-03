4187ae66eb83f79b707beea063996eadfade90cd
Yara Ghamlouch <ghamlouchyara68@gmail.com>
2025-03-03 11:27:43 -0500

    added cat

--- src/main/java/com/example/escape_the_lab/ui/rCircuit.java
+++ src/main/java/com/example/escape_the_lab/ui/rCircuit.java
@@ -1,12 +1,16 @@
 package com.example.escape_the_lab.ui;
 
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.FlowPane;
 
-public class Room1 {
+public class rCircuit {
     public Scene makeScene() {
         FlowPane pane = new FlowPane();
         Scene scene = new Scene(pane);
+
+        Image image = new Image("resources/cat.png");
+
 
         return scene;
     }
